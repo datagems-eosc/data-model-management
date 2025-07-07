29deb9ddcd7be0842c98e38409eeb68355b39091
Matteo Lissandrini <matteo.lissandrini@univr.it>
2025-07-07 17:24:49 +0200

    merged fast_api refactoring

--- dmm_api/resources/package.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-from flask import request, jsonify
-
-
-class Package(Resource):
-    def post(self):
-        data = request.get_json(force=True)
-        return jsonify(data)
