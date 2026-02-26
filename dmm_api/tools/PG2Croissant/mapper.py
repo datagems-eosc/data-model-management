35e1ce249aedbdd19683b619cae8e1a0c2994207
zoechevallier1 <zoe.chevallier@uvsq.fr>
2026-02-26 14:55:18 +0100

    Adding context

--- dmm_api/tools/PG2Croissant/mapper.py
+++ dmm_api/tools/PG2Croissant/mapper.py
@@ -1,6 +1,68 @@
 from dmm_api.tools.PG2Croissant.model import Dataset, FileObject, RecordSet, Field, ColumnStatistics
 
-
+CONTEXT = {
+    "@language": "en",
+    "@vocab": "https://schema.org/",
+    "access": "dg:access",
+    "citeAs": "cr:citeAs",
+    "column": "cr:column",
+    "conformsTo": "dct:conformsTo",
+    "cr": "http://mlcommons.org/croissant/",
+    "data": {
+      "@id": "cr:data",
+      "@type": "@json"
+    },
+    "dataType": {
+      "@id": "cr:dataType",
+      "@type": "@vocab"
+    },
+    "dct": "http://purl.org/dc/terms/",
+    "dg": "http://datagems.eu/TBD/",
+    "doi": "dg:doi",
+    "examples": {
+      "@id": "cr:examples",
+      "@type": "@json"
+    },
+    "extract": "cr:extract",
+    "field": "cr:field",
+    "fieldOfScience": "dg:fieldOfScience",
+    "fileObject": "cr:fileObject",
+    "fileProperty": "cr:fileProperty",
+    "fileSet": "cr:fileSet",
+    "format": "cr:format",
+    "histogram": "dg:histogram",
+    "includes": "cr:includes",
+    "isLiveDataset": "cr:isLiveDataset",
+    "jsonPath": "cr:jsonPath",
+    "key": "cr:key",
+    "max": "dg:max",
+    "md5": "cr:md5",
+    "mean": "dg:mean",
+    "median": "dg:median",
+    "min": "dg:min",
+    "missingCount": "dg:missingCount",
+    "missingPercentage": "dg:missingPercentage",
+    "parentField": "cr:parentField",
+    "path": "cr:path",
+    "rai": "http://mlcommons.org/croissant/RAI/",
+    "recordSet": "cr:recordSet",
+    "references": "cr:references",
+    "regex": "cr:regex",
+    "repeated": "cr:repeated",
+    "replace": "cr:replace",
+    "rowCount": "dg:rowCount",
+    "sc": "https://schema.org/",
+    "separator": "cr:separator",
+    "source": "cr:source",
+    "standardDeviation": "dg:standardDeviation",
+    "statistics": "dg:statistics",
+    "status": "dg:status",
+    "subField": "cr:subField",
+    "transform": "cr:transform",
+    "uniqueCount": "dg:uniqueCount",
+    "uploadedBy": "dg:uploadedBy",
+    "wd": "https://www.wikidata.org/wiki/"
+  }
 
 def map_to_croissant_dataset(datasets):
     for dataset in datasets:
@@ -12,7 +74,7 @@ def map_to_croissant_dataset(datasets):
             recordSets.append(map_recordSet(recordSet))
 
         dataset_dict = {    
-            "@context": "https://schema.org/",
+            "@context": CONTEXT,
             "@type": "Dataset",
             "@id": dataset.id,
             "distribution": distribution,
@@ -25,7 +87,10 @@ def map_to_croissant_dataset(datasets):
 def map_fileObjects(fileObject):
     fileObject_dict = {"@id": fileObject.id}
     for key,val in fileObject.properties.items():
-        fileObject_dict[key] = val
+        if key == "type":   
+            fileObject_dict["@type"] = val
+        else:
+            fileObject_dict[key] = val
     return fileObject_dict
 
 def map_recordSet(recordSet):
@@ -35,7 +100,10 @@ def map_recordSet(recordSet):
 
     recordSet_dict = {"@id": recordSet.id}
     for key,val in recordSet.properties.items():
-        recordSet_dict[key] = val
+        if key == "type":
+            recordSet_dict["@type"] = val   
+        else:   
+            recordSet_dict[key] = val
     recordSet_dict["fields"] = fields
     return recordSet_dict
 
@@ -47,7 +115,10 @@ def map_field(field):
     
     field_dict = {"@id": field.id}
     for key,val in field.properties.items():
-        field_dict[key] = val 
+        if key == "type":
+            field_dict["@type"] = val   
+        else:
+            field_dict[key] = val 
 
     field_dict["statistics"] = statistics  
     return field_dict
@@ -55,5 +126,8 @@ def map_field(field):
 def map_statistics(statistic):
     statistic_dict = {"@id": statistic.id}
     for key,val in statistic.properties.items():
-        statistic_dict[key] = val 
+        if key == "type":
+            statistic_dict["@type"] = val  
+        else:
+            statistic_dict[key] = val 
     return statistic_dict
